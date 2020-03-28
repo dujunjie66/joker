5a5cfea3fbde11247b2af8f80c7f549b513bb508
dujunjie66 <2568695790@qq.com>
2020-03-28 20:01:51 +0800

    3月完结

--- 2020/02.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('sp01').addEventListener('click',function(){
-    document.getElementById('mian').setAttribute('class','style01');
-  });
-  
-  document.getElementById('sp02').addEventListener('click',function(){
-    document.getElementById('mian').setAttribute('class','style02');
-  });
--- 2020/03.js
+++
@@ -1,8 +0,0 @@
-var count = 0;document.
-getElementById('ee').
-innerHTML = count;
-document.getElementById('ww').
-addEventListener('click',function(){  
-// 通过 变量名称=新值；来修改变量的内容    
-count = count + 1;    document.getElementById('ee').
-innerHTML = count;  });
